ec6404905e5f626b151c916f5608705f2d05a84c
Yahia Abdeldjalil <68236184+YahiaAbdeldjalilBenyahia@users.noreply.github.com>
2025-04-15 21:33:35 +0100

    Update traffic.py: run init.sh

--- utils/traffic.py
+++ utils/traffic.py
@@ -1,5 +1,17 @@
-def generate_urllc_traffic():
-    # Placeholder for traffic generation logic
-    print("Generating URLLC traffic...")
-    # Simulate success
-    return True
+import sys
+import subprocess
+def generate_urllc_traffic(path_to_init_sh="~/free5gc-compose/init.sh"):
+    try:
+        # Running the init.sh script
+        # the init.sh should be inside ~/free5gc-compose/ folder
+        print(f"Running: {path_to_init_sh}")
+        subprocess.run(["bash", path_to_init_sh], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+
+if __name__ == "__main__":
+        if len(sys.argv) == 2:
+            path_to_init_sh = sys.argv[1]
+            generate_urllc_traffic(path_to_init_sh)
+        else:
+            generate_urllc_traffic()
